912a00debb8b3c4a87ae7b5f6ba8e1007e8cde52
Manuel-Marquez1 <mmarquezlop.99@gmail.com>
2023-10-24 08:55:48 -0500

    Adding a new Js file for conditional logic

---
+++ script.js
@@ -0,0 +1,59 @@
+// The following will help us develop with better developer practices.
+"use strict"
+
+console.log("This is my nonprofit prototype webpage");
+alert("Welcome to my New page!!!");
+
+// One dynamic component - user input.
+//We need to store the resulting info in another variable. console.log(user_Name)
+
+const user_Name = prompt("What is your first name?");
+console.log(user_Name);
+
+const sauce = prompt("What is your favorite hot sauce brand?")
+console.log(sauce);
+
+const service = prompt("Do you like volunteering at nonprofits?")
+console.log(service);
+
+const permission = confirm("Do you have permission from an adult to eat spicy food?")
+console.log(permission)
+
+
+// Something new. In here you decide what you want to write to the document.
+//string concatenation
+document.write("Hi" + user_Name + "How is it going?");
+document.write("Your favorite hot sauce brand is" + sauce + "mine is Tabasco")
+document.write("You said" + service + "to volunteering!!!")
+
+//Conditional logic - capitalization matters in Js.
+
+
+
+if (user_Name == "Cameron"){
+    alert("Hey Cam! Please give me a good grade :)");
+} else if(user_Name == "cameron"){
+    alert("Hey Cam! Please give me a good grade");
+} else if (user_Name != "Cameron"){
+    alert ("Good to see you, this is your sign to go and get tacos!!!")
+}
+else {
+    alert("Feel free to donate to our cause. Link below!!!")
+}
+// Volunteering
+
+if (service == "No"){
+    alert("Hey it is never too late to become a volunteer!");
+} else {
+    alert("Congrats for volunteering! We are so proud of you!")
+}
+
+//spicy food
+if (permission = "Ok"){
+    alert("Wow, you are so brave!")
+}
+
+
+
+
+
