818bf1478dc56fb30e265119af46a300c87a5b53
Manuel-Marquez1 <mmarquezlop.99@gmail.com>
2023-10-27 06:15:03 -0500

    testing our Js. loop

--- script.js
+++ script.js
@@ -8,8 +8,15 @@ console.log(prompt)
 function place_order(){
     console.log("Your order will contain" + prompt + "tacos!!!");  //This line confirms your order.
     console.log("Your total is:" + prompt*5 + "USD."); //This line gives you the total $ spent.
-    console.group("Thank you for your purchase!!! 100% of your purchase will be dobated towards our caritable cause!"); //This line gives you a THANK YOU message.
+    for (let prompt = 0; prompt <= 1; i++){
+        console.log("Thank you for your purchase!!! 100% of your purchase will be dobated towards our caritable cause!")
+
+    }
+    
 }
 
 place_order()
+
+
+
 
