0d01c5566b8058b8c805f284e8d1eb9253caf7b4
Manuel-Marquez1 <mmarquezlop.99@gmail.com>
2023-10-27 06:55:08 -0500

    for this commit, our website displays a thank you message 20 times- everytime a customer or donor places an order

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 confirm = window.confirm("Hey there! Would you like to order some tacos?");
-prompt = window.prompt("How many tacos would ou like to order? (Maximum amount is 99 tacos")
+prompt = window.prompt("How many tacos would you like to order? (Maximum amount is 99 tacos")
 
 console.log(confirm)
 console.log(prompt)
@@ -8,7 +8,7 @@ console.log(prompt)
 function place_order(){
     console.log("Your order will contain" + prompt + "tacos!!!");  //This line confirms your order.
     console.log("Your total is:" + prompt*5 + "USD."); //This line gives you the total $ spent.
-    for (let prompt = 0; prompt < 100; prompt++){
+    for (let prompt = 0; prompt < 20; prompt++){
         console.log("Your tacos are being prepared!!! 100% of your purchase will be donated towards our caritable cause!", prompt);
 
     }
